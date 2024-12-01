71862a12bb3d7271166caecbd3444d40d2850401
Akshay10258 <135325303+Akshay10258@users.noreply.github.com>
2024-12-01 18:21:07 +0530

    Update auth.js

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -3,13 +3,13 @@ const {getuser}=require("../service/auth");
 async function restrictToLoggedinUserOnly(req,res,next){
     const userUid=req.cookies?.uid; // ? if u get error as properties of undefined pointing on req.cookie.....
     if(!userUid)
-        return res.json({ success: true, redirect: "http://localhost:5173/OwnerLogin" });
+        return res.json({ success: true, redirect: "https://be-my-pg.vercel.app/OwnerLogin" });
     
     const user=getuser(userUid);
   
 
     if(!user) 
-        return res.json({ success: true, redirect: "http://localhost:5173/OwnerLogin" });
+        return res.json({ success: true, redirect: "https://be-my-pg.vercel.app/OwnerLogin" });
     req.user=user;
     console.log(req.user._id);
     console.log("got the id ")
