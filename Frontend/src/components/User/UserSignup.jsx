dcbe871061a2fac4ab286b9a17a8a5eb72df4309
Akshay10258 <135325303+Akshay10258@users.noreply.github.com>
2024-12-01 17:40:21 +0530

    Update UserSignup.jsx

--- Frontend/src/components/User/UserSignup.jsx
+++ Frontend/src/components/User/UserSignup.jsx
@@ -78,7 +78,7 @@ function Signup() {
 
     const onSubmit = async (data) => {
         try {
-            const result = await fetch('https://be-my-pg-77p3.vercel.app/api/user/', {
+            const result = await fetch('https://be-my-pg-77p3.vercel.app/api/user', {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(data),
