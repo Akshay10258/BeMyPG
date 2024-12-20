8ebe58ea934a730391bf2bcb9088ac47d5432dca
Akshay10258 <akshayachar1913518@gmail.com>
2024-12-20 16:51:28 +0530

    Updated endpoint

--- Backend/index.js
+++ Backend/index.js
@@ -102,8 +102,8 @@ app.use("/rating",restrictToLoggedinPgUserOnly,rating);
 app.use("/ScheduleVisit",restrictToLoggedinPgUserOnly,ScheduleVisit);
 
 app.use("/",staticRoute);
-app.use("/api/owner",pgowner);      // means if url with /owner then call this
-app.use("/api/user",pguser);
+app.use("/owner",pgowner);      // means if url with /owner then call this
+app.use("/user",pguser);
 // MongoDB connection : BeMyPg (Name of the database)
 // mongoose.connect("mongodb://localhost:27017/BeMyPg")
 app.use((err, req, res, next) => {
